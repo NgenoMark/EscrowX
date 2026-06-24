2b73302663a3b6aeda499f2544675dbb520822a4
NgenoMark <kipchumbamark48@gmail.com>
2026-06-24 12:03:01 +0300

    patch : Implemented scheduling for payments

--- Application/esc-backend/src/main/java/com/example/escbackend/EscBackendApplication.java
+++ Application/esc-backend/src/main/java/com/example/escbackend/EscBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.escbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EscBackendApplication {
 
     public static void main(String[] args) {
